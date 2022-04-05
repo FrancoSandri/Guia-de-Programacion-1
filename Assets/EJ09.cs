48d53b78a02a5a1c7d7f7a41522b95b7fc03309b
FrancoSandri <46960579@est.ort.edu.ar>
2022-04-05 19:06:51 -0300

    EJ09 and EJ15 completed

---
+++ Assets/EJ09.cs
@@ -0,0 +1,32 @@
+﻿/*9. Realizá un programa que resuelva el siguiente problema:  
+Tres personas aportan diferente capital a una sociedad. Se desea saber qué porcentaje del  total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres.  
+Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
+Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ09 : MonoBehaviour
+{
+    public string Nombre1, Nombre2, Nombre3;
+    public float CapitalAportado_1, CapitalAportado_2, CapitalAportado_3;
+    float montototal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        montototal = CapitalAportado_1 + CapitalAportado_2 + CapitalAportado_3;
+        Debug.Log("Nombre: " + Nombre1+ ". Capital Aportado: $" + CapitalAportado_1 + ". Porcentaje del capital: %" + CapitalAportado_1 *100 / montototal + ". Monto total aportado: $" + montototal);
+        Debug.Log("Nombre: " + Nombre2 + ". Capital Aportado: $" + CapitalAportado_2 + ". Porcentaje del capital: %" + CapitalAportado_2 * 100 / montototal + ". Monto total aportado: $" + montototal);
+        Debug.Log("Nombre: " + Nombre3 + ". Capital Aportado: $" + CapitalAportado_3 + ". Porcentaje del capital: %" + CapitalAportado_3 * 100 / montototal+ ". Monto total aportado: $" + montototal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EJ15.cs
@@ -0,0 +1,65 @@
+﻿/*15. Existen dos reglas que identifican dos conjuntos de valores: 
+- El número es de un solo dígito.  
+- El número es impar.  
+A partir de estas reglas, creá un algoritmo que asigne a las variables booleanas estaEnA,  estaEnB, estaEnAmbos y noEstaEnNinguno el valor verdadero o falso, según corresponda,  para indicar si el valor ingresado pertenece al primer conjunto, al segundo, a ambos o a  ninguno, respectivamente. Definí un lote de prueba de varios números y probá el algoritmo,  escribiendo los resultados. 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ15 : MonoBehaviour
+{
+    public int Numero;
+    bool estaEnA;
+    bool estaEnB;
+    bool estaEnAmbos;
+    bool noEstaEnNinguno;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Numero <= 9)
+        {
+            estaEnA = true;
+        }
+        else
+        {
+            estaEnA = false;
+        }
+        if (Numero % 2 != 0)
+        {
+            estaEnB = true;
+        }
+        else
+        {
+            estaEnB = false;
+        }
+        if (estaEnA == true && estaEnB == true)
+        {
+            
+            estaEnAmbos = true;
+        }
+        else
+        {
+            estaEnAmbos = false;
+        }
+        if (estaEnA == false && estaEnB == false)
+        {
+            noEstaEnNinguno = true;
+        }
+        else
+        {
+            noEstaEnNinguno = false;
+        }
+        Debug.Log("Pertenece al conjunto A: " + estaEnA);
+        Debug.Log("Pertenece al conjunto B: " + estaEnB);
+        Debug.Log("Pertenece a ambos conjuntos: " + estaEnAmbos);
+        Debug.Log("No pertenece a ningun conjunto: " + noEstaEnNinguno);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
